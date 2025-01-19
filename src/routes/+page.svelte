feb0c2dc83b762935c667da31384180d4cfc24d5
Andre Koga <andrehkoga1@gmail.com>
2025-01-19 18:48:02 -0500

    add Sanity image URL builder and integrate image rendering in book and poem pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import urlFor from '$lib/sanityImage.js';
+
 	let { data } = $props();
 </script>
 
@@ -6,7 +8,14 @@
 	<h1>Blog Posts</h1>
 	<ul>
 		{#each data.books as book}
-			<li><a href={`/${book.slug.current}`}>{book.title}</a></li>
+			<li>
+				<a href={`/${book.slug.current}`}
+					><p>
+						{book.title}
+					</p>
+					<img src={urlFor(book.image).height(300).width(200).url()} alt="Book cover" />
+				</a>
+			</li>
 		{/each}
 	</ul>
 </main>
--- src/routes/[book]/+page.svelte
+++ src/routes/[book]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import urlFor from '$lib/sanityImage';
 	import { PortableText } from '@portabletext/svelte';
 
 	let { data } = $props();
@@ -7,6 +8,7 @@
 
 <main>
 	<h1>{data.book.title}</h1>
+	<img src={urlFor(data.book.image).width(200).url()} alt="Book cover" />
 	<PortableText value={data.book.preface} />
 	{#each data.book.pages as poem}
 		<a href={`${page.params.book}/${poem.slug.current}`}>
