f674d16031c405647dc054ec3f78b4fadabcabb6
Andre Koga <andrehkoga1@gmail.com>
2025-01-25 17:18:16 -0500

    Update Sanity environment variables to use private configuration

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@sanity/client';
-import { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } from '$env/static/public';
+import { SANITY_PROJECT_ID, SANITY_DATASET } from '$env/static/private';
 
 const client = createClient({
-    projectId: PUBLIC_SANITY_PROJECT_ID,
-    dataset: PUBLIC_SANITY_DATASET,
+    projectId: SANITY_PROJECT_ID,
+    dataset: SANITY_DATASET,
     apiVersion: '2025-01-19', // use a UTC date string
     useCdn: true, // `false` if you want to ensure fresh data
 });
