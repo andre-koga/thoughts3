922b1cd092ef240a9fa1375bc94f9234a7469367
Andre Koga <andrehkoga1@gmail.com>
2025-01-25 17:15:12 -0500

    Update Sanity client configuration to use environment variables

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@sanity/client';
+import { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } from '$env/static/public';
 
 const client = createClient({
-    projectId: 'kcartf0h', // replace with your project ID
-    dataset: 'production', // replace with your dataset name
+    projectId: PUBLIC_SANITY_PROJECT_ID,
+    dataset: PUBLIC_SANITY_DATASET,
     apiVersion: '2025-01-19', // use a UTC date string
     useCdn: true, // `false` if you want to ensure fresh data
 });
